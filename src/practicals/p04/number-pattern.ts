07303f70dd2a16a4ed3372d6522ead07ca394a47
Kamonphop Kunkaew <konthongkamonphop@gmail.com>
2026-01-21 16:07:52 +0700

    test

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,11 +1,12 @@
 const numberPattern=Number(process.argv[2]);
-let line="";
+
 for(let i=1;i<=numberPattern;i++){
-    line+=" ".repeat(numberPattern-i);
+    let line="";
+    line+=" ".repeat(numberPattern-i)+i
     if(i>=2){
-        for(let j=numberPattern;j>=i;i--){
-        line+=j
+        for(let j=i-1;j>=1;j--){
+            line+=j
+        }
     }
     console.log(line);
-}
 }
