5624826e8265408834e7c61b7021b6df41a52cc0
Kamonphop Kunkaew <konthongkamonphop@gmail.com>
2026-01-21 14:04:46 +0700

    test

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,20 @@
-// Write your code below
+const score = Number(process.argv[2]);
+let grade:string="";
+if(score<=80||score<=100){
+    grade="A";
+    console.log(`Grade is ${grade}`);
+}else if(score>=70){
+    grade="B";
+    console.log(`Grade is ${grade}`);
+}else if(score>=60){
+    grade="C";
+    console.log(`Grade is ${grade}`);
+}else if(score>=50){
+    grade="D";
+    console.log(`Grade is ${grade}`);
+}else if(score>=0){
+    grade="F"
+    console.log(`Grade is ${grade}`);
+}else{
+    console.log("Invalid input")
+}
